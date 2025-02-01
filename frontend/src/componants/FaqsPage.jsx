712095733b3386e7ba2bba9af1993d1a59021814
MashooqAli9140 <Faizsde9140@gmail.com>
2025-02-01 17:20:16 +0530

    fix: code format updated

--- frontend/src/componants/FaqsPage.jsx
+++ frontend/src/componants/FaqsPage.jsx
@@ -1,96 +1,136 @@
-import React, { useState } from 'react'
-import './FaqsPage.css';
+import React, { useState } from "react";
+import "./FaqsPage.css";
 
 const FaqsPage = () => {
-      const [ openans , setOpenAns ] = useState(false);
-      const [ question , setQuestion ] = useState("");
-      const [ answer , setanswer ] = useState("");
-      
+  const [openans, setOpenAns] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [answer, setanswer] = useState("");
 
-      async function AddNewFaq(e) {
-            e.preventDefault();
-            const newFaq = {
-                  question,
-                  answer
-            }
-            if( !question || !answer ) return alert("please add values")
-                  try {
-                        const response = await axios.post("http://localhost:8000/api/new-faq", newFaq, {
-                              Headers:{
-                                    "Content-Type": "application/json"
-                              },
-                              })
-                        const data = response.data;
-                        console.log("new faq saved");
-                  } catch (error) {
-                        console.log(error.message , "error while saving new faq");
-                  }
+  async function AddNewFaq(e) {
+    e.preventDefault();
+    const newFaq = {
+      question,
+      answer,
+    };
+    if (!question || !answer) return alert("please add values");
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/new-faq",
+        newFaq,
+        {
+          Headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = response.data;
+      console.log("new faq saved");
+    } catch (error) {
+      console.log(error.message, "error while saving new faq");
+    }
+  }
 
+  
 
-      }
-
-
-
-      
   return (
     <div>
-       {/* //NAVBAR SECTION  */}
-         <div id='Navbar'>
-               <div id='navleft'> 
-                    <h1 style={{ color:"#2C387A"}}> Bharat<span>FD</span>  </h1>
-               </div>
-               <div id='navright'>
-                     <p>Compare FD rates </p>
-                     <p>Contact us</p>
-               </div>
-         </div> {/* //NAVBAR SECTION  */}
-      
-     {/* //FAQS SECTION  */}
-     <div id='faqs-heading'>
-              <h2> Frequently Asked Questions </h2>
-     </div>
-    <div id='faqs-box-outer'>
-            <div id='faqs-box-inner'>
-                 <div id='language-btn-div'>
-                 <button id='language-btn'> English </button>
-                 <button id='language-btn'> Hindi </button>
-                 <button id='language-btn'> Bengali </button>
-                 <button id='language-btn'> Tamil </button>
-                 </div>
+      {/* //NAVBAR SECTION  */}
+      <div id="Navbar">
+        <div id="navleft">
+          <h1 style={{ color: "#2C387A" }}>
+            {" "}
+            Bharat<span>FD</span>{" "}
+          </h1>
+        </div>
+        <div id="navright">
+          <p>Compare FD rates </p>
+          <p>Contact us</p>
+        </div>
+      </div>{" "}
+      {/* //NAVBAR SECTION  */}
+      {/* //FAQS SECTION  */}
+      <div id="faqs-heading">
+        <h2> Frequently Asked Questions </h2>
+      </div>
+      <div id="faqs-box-outer">
+        <div id="faqs-box-inner">
+          <div id="language-btn-div">
+            <button id="language-btn"> English </button>
+            <button id="language-btn"> Hindi </button>
+            <button id="language-btn"> Bengali </button>
+            <button id="language-btn"> Tamil </button>
+          </div>
 
-                 <div id='q-a-div'>
-                     <button id='question-open-btn' onClick={ () => setOpenAns( (prev) => !prev ) }>
-                         <div id='ques-div'>
-                           <p style={{ color: openans? '#2C387A' : "black" }}> What is your name? </p>
-                           <p style={{ display:openans ? "none" : "block" }} > Show </p> 
-                           <p style={{ display:openans ? "block" : "none" }} > Hide </p> 
-                         </div>
-                     </button>
+          <div id="q-a-div">
+            <button
+              id="question-open-btn"
+              onClick={() => setOpenAns((prev) => !prev)}
+            >
+              <div id="ques-div">
+                <p style={{ color: openans ? "#2C387A" : "black" }}>
+                  {" "}
+                  What is your name?{" "}
+                </p>
+                <p style={{ display: openans ? "none" : "block" }}> Show </p>
+                <p style={{ display: openans ? "block" : "none" }}> Hide </p>
+              </div>
+            </button>
 
-                      <div id='ans-div' style={{ display: openans? "block" : "none" }}> <p> My name is BharatFD  </p> </div>
-                 </div>
-                 
+            <div id="ans-div" style={{ display: openans ? "block" : "none" }}>
+              {" "}
+              <p> My name is BharatFD </p>{" "}
             </div>
+          </div>
+        </div>
+      </div>
+      {/* //ADD NEW FAQS SECTION */}
+      <div id="new-faq-add-box">
+        <div>
+          <h2> Add new FAQS </h2>
+        </div>
+        <div id="add-ques-form">
+          <form onSubmit={(e) => AddNewFaq(e)}>
+            <input
+              onChange={(e) => setQuestion(e.target.value)}
+              style={{
+                outline: "none",
+                margin: "5px 0px 5px 0px",
+                width: "100%",
+                padding: "10px 15px 10px 15px",
+                border: "1px solid #212426",
+              }}
+              type="text"
+              placeholder="Type question here..."
+            />
+            <input
+              onChange={(e) => setanswer(e.target.value)}
+              style={{
+                outline: "none",
+                margin: "5px 0px 5px 0px",
+                width: "100%",
+                padding: "10px 15px 10px 15px",
+                border: "1px solid #212426",
+              }}
+              type="text"
+              placeholder="Type answer here..."
+            />
+            <button
+              type="submit"
+              style={{
+                outline: "none",
+                margin: "5px 0px 5px 0px",
+                width: "100%",
+                padding: "10px 15px 10px 15px",
+              }}
+              id="newfaq-add-btn"
+            >
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
+  );
+};
 
-    {/* //ADD NEW FAQS SECTION */}
-    <div id='new-faq-add-box'>
-           <div> 
-              <h2> Add new FAQS </h2>
-           </div>
-           <div id='add-ques-form'>
-                 <form onSubmit={ (e) => AddNewFaq(e) } > 
-                    <input onChange={ (e) => setQuestion(e.target.value)} style={{ outline:"none", margin:"5px 0px 5px 0px", width:"100%", padding:"10px 15px 10px 15px", border:'1px solid #212426'}} type="text" placeholder='Type question here...' />
-                    <input onChange={ (e) => setanswer(e.target.value)} style={{ outline:"none", margin:"5px 0px 5px 0px", width:"100%", padding:"10px 15px 10px 15px", border:'1px solid #212426'}} type="text" placeholder='Type answer here...' />
-                    <button type='submit' style={{ outline:"none", margin:"5px 0px 5px 0px", width:"100%", padding:"10px 15px 10px 15px"}} id='newfaq-add-btn'>
-                           Add
-                    </button>
-                 </form>
-           </div>
-    </div>
-
-    </div>
-  )
-}
-
-export default FaqsPage
+export default FaqsPage;
