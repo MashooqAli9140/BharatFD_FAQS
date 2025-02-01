ad7c76f0b3007c750b4b84467957852f8809bae6
MashooqAli9140 <Faizsde9140@gmail.com>
2025-02-01 18:01:05 +0530

    feat: faqs schema/model added in server

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,27 @@ const express = require('express');
 const app =  express()
 const bodyparser = require("body-parser");
 const connectDB = require('./config/ConnectDB.js');
+const FaqsSchema = require('./model/Faqs_model.js');
 
+
+//using body parser for getting data from upcoming url
 app.use( bodyparser.json());
+//connecting the mongodb
 connectDB();
 
-const PORT = process.env.PORT || 4000;
 
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 4000;
 app.listen( PORT, () => {
     console.log(`server running on PORT ${PORT}`)
 })
