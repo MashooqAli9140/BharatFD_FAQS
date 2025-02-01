25514059989140072de0a1a4fca52dbb37d94fbf
MashooqAli9140 <Faizsde9140@gmail.com>
2025-02-01 18:32:49 +0530

    feat: new faq post req created and saving only english languag and faqs model updated

--- Backend/model/Faqs_model.js
+++ Backend/model/Faqs_model.js
@@ -4,12 +4,14 @@ const Schema = mongoose.Schema;
 const FaqsSchema = new Schema( {
       question: { type: String , required: true },
       answer: { type: String , required: true },
-      question_hi: { type: String , required: true },
-      question_bn: { type: String , required: true },
-      question_ta: { type: String , required: true },
-      answer_hi: { type: String , required: true },
-      answer_bn: { type: String , required: true },
-      answer_ta: { type: String , required: true },
-})
+      question_hi: { type: String },
+      question_bn: { type: String },
+      question_ta: { type: String },
+      answer_hi: { type: String },
+      answer_bn: { type: String },
+      answer_ta: { type: String },
+},
+{collection :"BharatFD_FAQS"}
+)
 
 module.exports = mongoose.model( 'Faqs_model' ,FaqsSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const app =  express()
 const bodyparser = require("body-parser");
 const connectDB = require('./config/ConnectDB.js');
 const FaqsSchema = require('./model/Faqs_model.js');
+const cors = require('cors');
 
 
 //using body parser for getting data from upcoming url
@@ -11,6 +12,31 @@ app.use( bodyparser.json());
 //connecting the mongodb
 connectDB();
 
+app.use( cors( {
+    origin: [
+        'http://localhost:5173', //for development
+    ],
+    methods:[ 'GET', 'PUT' , 'POST' , 'DELETE'],
+    credentials:true, //Allow cookie if needed
+}))
+
+
+
+//created post req for new FAQ's
+app.post("/api/new-faq", async (req, res) => {
+    const { question , answer } = req.body;
+    if( !question || !answer ) return res.status(400).json({"msge":"please provide Q and A"})
+    
+    try {
+        const SaveFaqs = await FaqsSchema.create({
+            question,
+            answer
+        })
+        return res.status(201).json({"msge":"new faq saved success"})
+    } catch (error) {
+        return res.status(500).json({"msge":"error while saving faq"});
+    }  
+})
 
 
 
