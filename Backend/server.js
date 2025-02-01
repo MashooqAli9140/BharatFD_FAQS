630e6d065a3464c0276a4047edfdfb899ce3c6da
MashooqAli9140 <Faizsde9140@gmail.com>
2025-02-01 17:36:06 +0530

    fix: bodyparser used and run the BE on PORT 8000

--- Backend/server.js
+++ Backend/server.js
@@ -2,3 +2,12 @@ require("dotenv").config();
 const express = require('express');
 const app =  express()
 const bodyparser = require("body-parser");
+
+app.use( bodyparser.json());
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen( PORT, () => {
+    console.log(`server running on PORT ${PORT}`)
+})
